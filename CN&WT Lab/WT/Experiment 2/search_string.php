e5aed70d190df0784b1a7c40a87b2055de1806c8
harika1201 <60428829+harika1201@users.noreply.github.com>
2021-06-10 18:52:36 +0530

    Add files via upload

---
+++ CN&WT Lab/WT/Experiment 2/search_string.php
@@ -0,0 +1,17 @@
+<?php
+
+$str1 = 'Welcome to WT Lab';
+if (strpos($str1,'Lab') !== false) 
+ {
+    echo 'The specific word is present.';
+ }
+else
+ {
+    echo 'The specific word is not present.';
+ }
+
+?>
+ 
+ Output:
+
+The specific word is present.
